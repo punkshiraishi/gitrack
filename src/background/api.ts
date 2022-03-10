9c50262e2f2e15da49f0093b26068740533f5858
shiraishi <shiraishi_yuma@uniquevision.co.jp>
2022-03-10 23:13:12 +0900

    fix: clockify API client を切り出した

--- src/background/api.ts
+++ src/background/api.ts
@@ -6,20 +6,29 @@ async function getOptions() {
   )
 }
 
-export async function postTimeentries() {
+async function fetchClocify(method: 'get' | 'post', path: string, body: Record<string, string | Date>) {
   const { clockifyToken, clockifyWorkspace } = await getOptions()
-  const url = `https://api.clockify.me/api/v1/workspaces/${clockifyWorkspace}/time-entries`
+  const url = `https://api.clockify.me/api/v1/workspaces/${clockifyWorkspace}/${path}`
 
   const response = await fetch(url, {
-    method: 'post',
+    method,
     headers: {
       'X-Api-Key': clockifyToken,
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({
-      start: new Date(),
-    }),
+    body: JSON.stringify(body),
     credentials: 'include',
   })
+
+  return await response.json()
+}
+
+export async function postTimeentries() {
+  const response = await fetchClocify(
+    'post',
+    'time-entries',
+    { start: new Date() },
+  )
+
   return await response.json()
 }
