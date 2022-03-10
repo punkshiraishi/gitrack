0fab5a810d0d94b10e3dd2a802298e556dd45db3
shiraishi <shiraishi_yuma@uniquevision.co.jp>
2022-03-11 00:24:05 +0900

    delete: sample code in popup

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,10 +1,5 @@
 <template>
   <main class="w-[300px] px-4 py-5 text-center text-gray-700 flex flex-col space-y-3">
-    <Logo />
-    <div>Popup</div>
-    <p class="mt-2 opacity-50">
-      This is the popup page
-    </p>
     <button class="btn mt-2" @click="openOptionsPage">
       Open Options
     </button>
