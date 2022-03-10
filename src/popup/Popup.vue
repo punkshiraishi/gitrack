2b779a8a28223c2287a814ca978ed90bb5618a4e
shiraishi <shiraishi_yuma@uniquevision.co.jp>
2022-03-11 00:21:53 +0900

    fix: infer issue id and repository name from url

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -11,11 +11,35 @@
     <button class="btn mt-2" @click="startTracking">
       Start Tracking
     </button>
+    {{ issueId }}
+    {{ repository }}
   </main>
 </template>
 
 <script setup lang="ts">
 import { sendMessage } from 'webext-bridge'
+
+const issueId = ref('')
+const repository = ref('')
+
+onMounted(async() => {
+  const tabs = await browser.tabs.query({ active: true, lastFocusedWindow: true })
+  const url = tabs[0].url
+
+  if (url) {
+    const matchedIssueId = url.match(/\/-\/issues\/(\d.+)/)
+
+    if (matchedIssueId) {
+      issueId.value = matchedIssueId[1]
+    }
+
+    const machedRepository = url.match(/\/([^\/]+)\/-\/issues/)
+
+    if (machedRepository) {
+      repository.value = machedRepository[1]
+    }
+  }
+})
 
 function openOptionsPage() {
   browser.runtime.openOptionsPage()
